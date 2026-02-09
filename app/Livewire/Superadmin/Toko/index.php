07bf980857e808e603653c445fcfcd9ee8ecae73
Indrawan-maker <indrawanismail0@gmail.com>
2026-02-09 13:27:27 +0800

    add edit user

--- app/Livewire/Superadmin/Toko/index.php
+++ app/Livewire/Superadmin/Toko/index.php
@@ -2,17 +2,16 @@
 namespace App\Livewire\Superadmin\Toko;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 use Livewire\WithPagination;
-
 class Index extends Component
 {
     use WithPagination;
     protected $paginationTheme = 'bootstrap';
     public $paginate = '10';
     public $search  = '';
-    public $nama;
-    public $email;
+    public $nama, $email, $role, $password, $password_confirmation, $user_id;
     public function render()
     {
         $data = array(
@@ -27,18 +26,82 @@ class Index extends Component
         $this->resetValidation();
         $this->reset([
             'nama',
-            'email'
+            'email',
+            'role',
+            'password',
+            'password_confirmation'
         ]);
     }
 
     public function store()
     {
         $this->validate([
-            'nama' => 'required',
-            'email' => 'required'
+            'nama'                              => 'required',
+            'email'                             => 'required|email|unique:users,email',
+            'role'                              => 'required',
+            'password'                          => 'required|min:8|confirmed',
+            'password_confirmation'             => 'required'
         ],[
-            'nama.required' => 'nama tidak boleh kosong!',
-            'email.required' => 'email tidak boleh kosong!'
+            'nama.required'                                 => 'nama tidak boleh kosong!',
+            'email.required'                                => 'email tidak boleh kosong!',
+            'email.unique'                                => 'Email Sudah Terdaftar!',
+            'email.email'                                   => 'email tidak valid!',
+            'role.required'                                 => 'role tidak boleh kosong!',
+            'password.required'                             => 'password tidak boleh kosong!',
+            'password.min:8'                                => 'password minimal dari 8 karakter!',
+            'password.confirmed'                            => 'password tidak konfirmasi tidak SAMA!',
+            'password_confirmation.required'                => 'konfirmasi password tidak boleh kosong!'
         ]);
+
+        try {
+            $user                           = new User;
+            $user->nama                     = $this->nama;
+            $user->email                    = $this->email;
+            $user->role                     = $this->role;
+            $user->password                 = Hash::make($this->password);
+            $user->save();
+            $this->dispatch('closeCreateModal');
+            } catch (\Exception $e) {
+        $this->dispatch('errorWhenCreateUser');
     }
+        }
+
+        public function edit($id) {
+            $this->resetValidation();
+            $user = User::findOrFail($id);
+            $this->nama = $user->nama;
+            $this->email = $user->email;
+            $this->role = $user->role;
+            $this->user_id = $user->id;
+        }
+
+        public function update($id) {
+            $user = User::findOrFail($id);
+            $this->validate([
+            'nama'                              => 'required',
+            'email'                             => 'required|email|unique:users,email,'.$id,
+            'role'                              => 'required',
+            'password'                          => 'nullable|min:8|confirmed',
+        ],[
+            'nama.required'                                 => 'nama tidak boleh kosong!',
+            'email.required'                                => 'email tidak boleh kosong!',
+            'email.email'                                   => 'email tidak valid!',
+            'role.required'                                 => 'role tidak boleh kosong!',
+            'password.required'                             => 'password tidak boleh kosong!',
+            'password.min:8'                                => 'password minimal dari 8 karakter!',
+            'password.confirmed'                            => 'password tidak konfirmasi tidak SAMA!',
+        ]);
+
+        // try {
+            // $user                           = new User;
+            // $user->nama                     = $this->nama;
+            // $user->email                    = $this->email;
+            // $user->role                     = $this->role;
+            // $user->password                 = Hash::make($this->password);
+            // $user->save();
+            // $this->dispatch('closeCreateModal');
+    //         } catch (\Exception $e) {
+    //     $this->dispatch('errorWhenCreateUser');
+    // }
+        }
 }
--- resources/views/livewire/superadmin/toko/index.blade.php
+++ resources/views/livewire/superadmin/toko/index.blade.php
@@ -1,117 +1,151 @@
 <div>
-      <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1><i class="fas fa-store mr-1"></i>{{ $title }}</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#"><i class="fas fa-home mr-1"></i>Dashboard</a></li>
-              <li class="breadcrumb-item active"><i class="fas fa-store mr-1"></i>{{ $title }}</li>
-            </ol>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <!-- Main content -->
-    <section class="content">
-
-      <!-- Default box -->
-      <div class="card">
-        <div class="card-header">
-          <div class="d-flex justify-content-between">
-
-            <div>
-              <button wire:click='create' class="btn btn-sm btn-primary" data-toggle="modal" data-target="#createModal">
-                <i class="fas fa-plus mr-1"></i>Tambah Data
-              </button>
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1><i class="fas fa-store mr-1"></i>{{ $title }}</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#"><i class="fas fa-home mr-1"></i>Dashboard</a>
+                            </li>
+                            <li class="breadcrumb-item active"><i class="fas fa-store mr-1"></i>{{ $title }}</li>
+                        </ol>
+                    </div>
+                </div>
             </div>
-            <div class="btn-group dropleft">
-              <button type="button" class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                <i class="fas fa-print mr-1"></i> Cetak
-              </button>
-              <div class="dropdown-menu">
-                <!-- Dropdown menu links -->
-                 <a class="dropdown-item text-success" href="#"><i class="fas fa-file-excel mr-1"></i>Excel</a>
-                 <a class="dropdown-item text-danger" href="#"><i class="fas fa-file-pdf mr-1"></i>PDF</a>
-              </div>
+        </section>
+
+        <!-- Main content -->
+        <section class="content">
+
+            <!-- Default box -->
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex justify-content-between">
+
+                        <div>
+                            <button wire:click='create' class="btn btn-sm btn-primary" data-toggle="modal"
+                                data-target="#createModal">
+                                <i class="fas fa-plus mr-1"></i>Tambah Data
+                            </button>
+                        </div>
+                        <div class="btn-group dropleft">
+                            <button type="button" class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown"
+                                aria-expanded="false">
+                                <i class="fas fa-print mr-1"></i>Cetak
+                            </button>
+                            <div class="dropdown-menu">
+                                <!-- Dropdown menu links -->
+                                <a class="dropdown-item text-success" href="#"><i
+                                        class="fas fa-file-excel mr-1"></i>Excel</a>
+                                <a class="dropdown-item text-danger" href="#"><i
+                                        class="fas fa-file-pdf mr-1"></i>PDF</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3 d-flex justify-content-between">
+                        <div class="col-2">
+                            <select wire:model.live="paginate" class="form-control">
+                                <option value="10">10</option>
+                                <option value="25">25</option>
+                                <option value="50">50</option>
+                                <option value="100">100</option>
+                            </select>
+                        </div>
+                        <div class="col-4">
+                            <input wire:model.live="search" type="text" class="form-control"
+                                placeholder="Pencarian...">
+                        </div>
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Role</th>
+                                    <th>
+                                        <i class="fas cog">
+                                        </i>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($user as $item)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $item->nama }}</td>
+                                        <td>{{ $item->email }}</td>
+                                        @if ($item->role == 'superadmin')
+                                            <td>
+                                                <span class="badge badge-primary">
+                                                    {{ $item->role }}
+                                                </span>
+                                            </td>
+                                        @elseif($item->role == 'admin')
+                                            <th>
+                                                <span class="badge badge-light">
+                                                    {{ $item->role }}
+                                                </span>
+                                            </th>
+                                        @endif
+                                        <td>
+                                            <button  wire:click='edit({{ $item->id }})' data-toggle="modal"
+                                data-target="#editModal"class="btn btn-sm btn-warning">
+                                                <i class="fas fa-edit"></i>
+                                            </button>
+                                            <button class="btn btn-sm btn-danger">
+                                                <i class="fas fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        {{ $user->links() }}
+                    </div>
+                </div>
             </div>
- </div>
-  </div>
-    <div class="card-body">
-      <div class="mb-3 d-flex justify-content-between">
-        <div class="col-2">
-          <select wire:model.live="paginate" class="form-control">
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </select>
-        </div>
-        <div class="col-4">
-          <input wire:model.live="search" type="text" class="form-control" placeholder="Pencarian...">
-        </div>
-      </div>
-        <div class="table-responsive">
-          <table class="table table-hover">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Nama</th>
-                <th>Email</th>
-                <th>Role</th>
-                <th>
-                  <i class="fas cog">
+        </section>
 
-                  </i>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              @foreach ($user as $item)
-                  <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $item->nama }}</td>
-                    <td>{{ $item->email }}</td>
-                    @if ($item->role == 'superadmin')
-                    <td>
-                      <span class="badge badge-primary">  
-                        {{ $item->role }}
-                      </span>
-                    </td>
-                    @elseif($item->role == 'admin')
-                    <th>
-                      <span class="badge badge-light">
-                        {{ $item->role }}  
-                      </span>    
-                    </th>
-                    @endif
-                    <td>
-                      <button class="btn btn-sm btn-warning">
-                        <i class="fas fa-edit"></i>
-                      </button>
-                      <button class="btn btn-sm btn-danger">
-                        <i class="fas fa-trash"></i>
-                      </button>
-                    </td>
-                  </tr>
-              @endforeach
-            </tbody>
-          </table>
-          {{ $user->links() }}
-        </div>
-    </div>
+        {{-- CREATE MODAL --}}
+        @include('livewire.superadmin.toko.create')
+        {{-- CREATE MODAL --}}
 
-    </div>
+        {{-- CLOSE CREATE MODAL --}}
+        @script
+            <script>
+                $wire.on('closeCreateModal', () => {
+                    $('#createModal').modal('hide');
 
-    </section>
+                    Swal.fire({
+                        title: "Sukes!",
+                        text: "Data Berhasil Ditambah!",
+                        icon: "success"
+                    });
+                });
 
-    {{-- CREATE MODAL --}}
-    @include('livewire.superadmin.toko.create')
-    {{-- CREATE MODAL --}}
+                  $wire.on('errorWhenCreateUser', () => {
+                    $('#createModal').modal('hide');
+
+                    Swal.fire({
+                        title: "Gagal!",
+                        text: "Terjadi kesalahan saat menyimpan data!",
+                        icon: "error"
+                    });
+                });
+            </script>
+        @endscript
+        {{-- CLOSE CREATE MODAL --}}
+
+        {{-- EDIT MODAL --}}
+        @include('livewire.superadmin.toko.edit')
+        {{-- EDIT MODAL --}}
     </div>
 </div>
